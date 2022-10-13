f4f6e78ec05fd6b6c31abe928285a588371495bd
Ruslan_Kasyanovich <ruslank906@gmail.com>
2022-10-13 23:04:56 +0300

    Initial commit

---
+++ Assets/Move.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Move
+{
+    public class Move : MonoBehaviour
+    {
+        public float speed = 2.0f;
+        public float accuracy = 1f;
+        public Transform goal;
+
+
+        void Start()
+        {
+            
+        }
+
+        void LateUpdate()
+        {
+            Vector3 direction = goal.position - this.transform.position;
+            if (direction.magnitude > accuracy)
+            {
+                this.transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+            }
+            this.transform.LookAt(goal.position);
+        }
+    }
+}
+
